29bd01a591935d9205461148cd76668ce46a8633
Michell <michel_leone@exactus.com.br>
2024-10-11 09:32:48 -0300

    crud angular spring

---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = '/assets/courses.json';
+
+  constructor(private httpClient: HttpClient) { }
+
+  list() {
+       return this.httpClient.get<Course[]>(this.API);
+  }
+}
